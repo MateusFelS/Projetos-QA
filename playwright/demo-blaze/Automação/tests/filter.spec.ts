df74792711e219723b0ecab76fadbe88d28696c1
Mateus Santos <122156760+MateusFelS@users.noreply.github.com>
2026-09-03 22:28:54 -0300

    Delete playwright directory

--- playwright/demo-blaze/Automação/tests/filter.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test'
-
-test('Filtrar por Laptops', async ({page}) => {
-    await page.goto('https://www.demoblaze.com')
-    await page.locator('#itemc').filter({hasText:'Laptops'}).click()
-    await expect(page.locator('.hrefch').first()).toHaveText('Sony vaio i5')
-})
