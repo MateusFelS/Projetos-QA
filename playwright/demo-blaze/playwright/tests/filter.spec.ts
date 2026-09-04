b8f0d0eee37ded0debe833287d34cbfb6993531b
Mateus Santos <122156760+MateusFelS@users.noreply.github.com>
2026-09-03 23:20:25 -0300

    Update filter.spec.ts

--- playwright/demo-blaze/playwright/tests/filter.spec.ts
+++ playwright/demo-blaze/playwright/tests/filter.spec.ts
@@ -1,7 +1,12 @@
 import { test, expect } from '@playwright/test'
+import { HomePage } from './pages/home.page'
 
-test('Filtrar por Laptops', async ({page}) => {
-    await page.goto('https://www.demoblaze.com')
-    await page.locator('#itemc').filter({hasText:'Laptops'}).click()
-    await expect(page.locator('.hrefch').first()).toHaveText('Sony vaio i5')
+test('Filtrar por Laptops', async ({ page }) => {
+  const home = new HomePage(page)
+
+  await home.acessar()
+  await home.selecionarCategoria('Laptops')
+
+  await expect(page.locator('.hrefch').first())
+    .toHaveText('Sony vaio i5')
 })
