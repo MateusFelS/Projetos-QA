bc0bc15aa24dbd3ef33d01d8b70205a8bf87b406
Mateus Santos <122156760+MateusFelS@users.noreply.github.com>
2026-09-03 23:18:32 -0300

    Create test-data.ts

---
+++ playwright/demo-blaze/playwright/tests/data/test-data.ts
@@ -0,0 +1,27 @@
+export const usuarios = {
+  valido: {
+    username: 'testeqa_01',
+    password: '123321',
+  },
+  invalido: {
+    username: 'teste',
+    password: '123',
+  },
+}
+
+export const pedido = {
+  nome: 'João Teste',
+  pais: 'Brasil',
+  cidade: 'São Paulo',
+  cartao: '1234123412341234',
+  mes: '12',
+  ano: '2026',
+}
+
+export const contato = {
+  email: 'teste@teste.com',
+  nome: 'Teste Name',
+  mensagem: 'Mensagem',
+}
+
+export const produto = 'Samsung galaxy s6'
